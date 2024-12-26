42f02a8b4d9e005df762526bccda65b5a83fa5a4
David Simmerman <davidsimmermancs@gmail.com>
2024-12-26 12:08:38 -0500

    add sorting to headers

--- client/src/components/RankingsList/RankingsList.tsx
+++ client/src/components/RankingsList/RankingsList.tsx
@@ -1,31 +1,70 @@
 import useFetchKenpom from '@/hooks/useFetchKenpom';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
-import { KenpomTeam } from '@/types/kenpom';
+import { KenpomTeam, WinLossRecord } from '@/types/kenpom';
 
 export default function RankingsList() {
 	const { teams: teamIndex, loading } = useFetchKenpom();
+	const [sorting, setSorting] = useState<string>();
 
-	const teams = useMemo(() => teamIndex && Object.values(teamIndex), [teamIndex]);
+	const teams = useMemo(
+		() =>
+			teamIndex &&
+			Object.values(teamIndex).sort((a, b) => {
+				if (!sorting) return 0;
+
+				const rankKey = (sorting + '_rank') as keyof KenpomTeam;
+				const sortKey = (a[rankKey] ? rankKey : sorting) as keyof KenpomTeam;
+
+				if (sortKey === 'win_loss') {
+					return getWinLossPct(b.win_loss) - getWinLossPct(a.win_loss);
+				} else if (typeof a[sortKey] === 'string') {
+					return a[sortKey].localeCompare(b[sortKey] as string);
+				} else if (sortKey.endsWith('rank')) {
+					return (a[sortKey] as number) - (b[sortKey] as number);
+				} else {
+					return (b[sortKey] as number) - (a[sortKey] as number);
+				}
+			}),
+		[teamIndex, sorting]
+	);
 	const headers = useMemo(() => teams && Object.keys(teams[0]).filter(h => !h.endsWith('_rank')), [teams]);
+
+	function getWinLossPct(win_loss: WinLossRecord) {
+		const [winsString, lossesString] = win_loss.split('-');
+		const wins = parseInt(winsString);
+		const losses = parseInt(lossesString);
+
+		return wins / (wins + losses);
+	}
 
 	return (
 		<div>
 			{!loading && (
 				<Table>
 					<TableHeader>
-						<TableRow>{headers?.map(h => <TableHead key={`kp_header_item_${h}`}>{h}</TableHead>)}</TableRow>
+						<TableRow>
+							{headers?.map(h => (
+								<TableHead
+									className={`capitalize hover:bg-muted/50 cursor-pointer text-center ${sorting === h && 'underline text-white'}`}
+									onClick={() => setSorting(h)}
+									key={`kp_header_item_${h}`}
+								>
+									{h.replaceAll('_', ' ')}
+								</TableHead>
+							))}
+						</TableRow>
 					</TableHeader>
 					<TableBody>
 						{teams?.map(team => (
-							<TableRow key={`kp_rankings_table_team_${team.name}`}>
+							<TableRow key={`kp_rankings_table_team_${team.team}`}>
 								{Object.entries(team).map(([header, rank]) => {
 									if (header.endsWith('_rank')) return null;
 
 									const rankHeader = (header + '_rank') as keyof KenpomTeam;
 
 									return (
-										<TableCell className="text-center" key={`kp_rankings_table_team_${team.name}_${header}`}>
+										<TableCell className="text-center" key={`kp_rankings_table_team_${team.team}_${header}`}>
 											{team[rankHeader] ? (
 												<>
 													{rank}
