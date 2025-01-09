43e75141e2c8794885f6a25b07bfd73a9aecc931
David Simmerman <davidsimmermancs@gmail.com>
2025-01-08 21:19:28 -0500

    Make headers pinned to top

--- client/src/components/RankingsList/RankingsList.tsx
+++ client/src/components/RankingsList/RankingsList.tsx
@@ -3,6 +3,7 @@ import { useMemo, useState } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
 import { KenpomTeam, WinLossRecord } from '@/types/kenpom';
 import { MdOutlineTrendingDown, MdOutlineTrendingFlat, MdOutlineTrendingUp } from 'react-icons/md';
+import { ScrollArea } from '../ui/scroll-area';
 
 export default function RankingsList() {
 	const { teams: teamIndex, loading } = useFetchKenpom();
@@ -56,70 +57,72 @@ export default function RankingsList() {
 	};
 
 	return (
-		<div>
+		<>
 			{!loading && (
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableHead
-								className={`capitalize hover:bg-muted/50 cursor-pointer text-center ${sorting === 'price' && 'underline text-white'}`}
-								onClick={() => setSorting('price')}
-								key={`kp_header_item_price`}
-							>
-								$
-							</TableHead>
-							{headers?.map(h => (
+				<ScrollArea className="h-screen rounded-md border w-fit overflow-x-auto">
+					<Table>
+						<TableHeader className="sticky top-0 bg-secondary ">
+							<TableRow>
 								<TableHead
-									className={`capitalize hover:bg-muted/50 cursor-pointer text-center ${sorting === h && 'underline text-white'}`}
-									onClick={() => setSorting(h)}
-									key={`kp_header_item_${h}`}
+									className={`capitalize hover:bg-muted/50 cursor-pointer text-center ${sorting === 'price' && 'underline text-white'}`}
+									onClick={() => setSorting('price')}
+									key={`kp_header_item_price`}
 								>
-									{h.replaceAll('_', ' ')}
+									$
 								</TableHead>
-							))}
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{teams?.map(team => (
-							<TableRow key={`kp_rankings_table_team_${team.team}`}>
-								<TableCell
-									className="text-center flex w-full justify-between items-center"
-									key={`kp_rankings_table_team_${team.team}_price`}
-								>
-									<span>
-										$&nbsp;
-										{team.price}
-									</span>
-									{Object.values(trendIconMap)[Math.floor(Math.random() * 3)]}
-								</TableCell>
-								{Object.entries(team).map(([header, rank]) => {
-									if (header.endsWith('_rank') || header === 'price') return null;
-
-									const rankHeader = (header + '_rank') as keyof KenpomTeam;
-
-									return (
-										<>
-											<TableCell
-												className="text-center"
-												key={`kp_rankings_table_team_${team.team}_${header}`}
-											>
-												{team[rankHeader] ? (
-													<>
-														{rank}
-														<span className="ml-1 text-xs text-gray-600">{team[rankHeader]}</span>
-													</>
-												) : (
-													rank
-												)}
-											</TableCell>
-										</>
-									);
-								})}
+								{headers?.map(h => (
+									<TableHead
+										className={`capitalize hover:bg-muted/50 cursor-pointer text-center ${sorting === h && 'underline text-white'}`}
+										onClick={() => setSorting(h)}
+										key={`kp_header_item_${h}`}
+									>
+										{h.replaceAll('_', ' ')}
+									</TableHead>
+								))}
 							</TableRow>
-						))}
-					</TableBody>
-				</Table>
+						</TableHeader>
+						<TableBody>
+							{teams?.map(team => (
+								<TableRow key={`kp_rankings_table_team_${team.team}`}>
+									<TableCell
+										className="text-center flex w-full justify-between items-center"
+										key={`kp_rankings_table_team_${team.team}_price`}
+									>
+										<span>
+											$&nbsp;
+											{team.price}
+										</span>
+										{Object.values(trendIconMap)[Math.floor(Math.random() * 3)]}
+									</TableCell>
+									{Object.entries(team).map(([header, rank]) => {
+										if (header.endsWith('_rank') || header === 'price') return null;
+
+										const rankHeader = (header + '_rank') as keyof KenpomTeam;
+
+										return (
+											<>
+												<TableCell
+													className="text-center"
+													key={`kp_rankings_table_team_${team.team}_${header}`}
+												>
+													{team[rankHeader] ? (
+														<>
+															{rank}
+															<span className="ml-1 text-xs text-gray-600">{team[rankHeader]}</span>
+														</>
+													) : (
+														rank
+													)}
+												</TableCell>
+											</>
+										);
+									})}
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</ScrollArea>
 			)}
-		</div>
+		</>
 	);
 }
